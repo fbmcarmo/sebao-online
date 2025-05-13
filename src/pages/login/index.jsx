9c1a17bf72702a8967f86232958c5c632a66dd2d
unknown <davirb21@gmail.com>
2025-05-13 17:36:54 -0300

    ajuste loginpage

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,6 @@
 import PageWrapper from "@/components/PageWrapper";
 
-export default function Login() {
+export default function LoginPage() {
   return (
     <div className="mih-h-screen flex flex-col items-center justify-center bg-gray-100">
       <Header />
