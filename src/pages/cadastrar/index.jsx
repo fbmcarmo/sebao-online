7e96b65fd05733761bec2d0b6fd6db7b403be9e1
Michele In The Code <miinthecode@gmail.com>
2025-05-14 10:52:36 -0300

    cadastro

--- src/pages/cadastrar/index.jsx
+++ src/pages/cadastrar/index.jsx
@@ -1,9 +1,59 @@
 import PageWrapper from "@/components/PageWrapper";
 
-export default function Cadastrar(){
-    return (
-        <PageWrapper>
-            <p>Essa é a página de cadastro</p>
-        </PageWrapper>
-    )
+export default function Login() {
+  return (
+  <PageWrapper>
+    <div className="w-full h-[80vh] flex items-center justify-center">
+      <form className="bg-white p-6 rounded flex flex-col gap-2 shadow-md w-full max-w-sm ">
+        <h1 className="text-xl font-bold mb-4 text-center text-[#884211]">
+          Crie sua conta
+        </h1>
+        <label className="block te xt-sm font-bold text-gray-700">Nome</label>
+        <input
+          type="email"
+          className="w-full p-2 border rounded mb-4 focus:outline-none focus:ring-2 focus:ring-[#884211]"
+          placeholder="Nome"
+          required
+        />
+        <label className="block text-sm font-bold text-gray-700">Email</label>
+        <input
+          type="email"
+          className="w-full p-2 border rounded mb-4 focus:outline-none focus:ring-2 focus:ring-[#884211]"
+          placeholder="seu@email.com"
+          required
+        />
+        <label className="block text-sm font-bold text-gray-700">Senha</label>
+        <input
+          type="password"
+          placeholder="******"
+          className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-[#884211]"
+          required
+        />
+        <label className="block text-sm font-bold text-gray-700">Confirme sua Senha</label>
+        <input
+          type="password"
+          placeholder="******"
+          className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-[#884211]"
+          required
+        />
+        <div className="mt-6">
+          <button
+            type="submit"
+            className="w-full bg-[#884211] text-white p-2 rounded houver:bg-[#6f350f] transition"
+          >
+            Entrar
+          </button>
+        </div>
+        <div className="mt-4">
+          <p className="text-center text-[#976460]">Já tem uma conta?
+            <button className="text-[#976460] font-bold cursor-pointer pl-2" 
+            onClick={() => window.location.href = "/cadastrar"}>
+              Entre
+            </button>
+          </p>
+        </div>
+      </form>
+    </div>
+  </PageWrapper>
+  );
 }
