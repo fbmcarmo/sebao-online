ed40881643c9ad0d37488446130d7acd85491758
fbmcarmo <fbrunomcarmo@gmail.com>
2025-05-22 09:13:25 -0300

    Atualizei os dados de livros

--- src/data/livros.js
+++ src/data/livros.js
@@ -1,8 +1,8 @@
-import harryPotter from "/public/harryPotter.jpg"
-import milnovecentoseoitentaequatro from "/public/milnovecentoseoitentaequatro.jpg"
-import inglesCard from "/public/inglesCard.jpg"
-import quarentaEoitoLeisDoPoder from "/public/quarentaEoitoLeisDoPoder.jpg"
-import javascript from "/public/javascript.jpg"
+import harryPotter from "/public/harryPotter.jpg";
+import milnovecentoseoitentaequatro from "/public/milnovecentoseoitentaequatro.jpg";
+import inglesCard from "/public/inglesCard.jpg";
+import quarentaEoitoLeisDoPoder from "/public/quarentaEoitoLeisDoPoder.jpg";
+import javascript from "/public/javascript.jpg";
 
 const livros = [
   {
@@ -13,6 +13,8 @@ const livros = [
     autor: "J. K. Rowling",
     preco: "R$ 20,00",
     categoria: "Fantasia",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
   },
   {
     id: 2,
@@ -22,6 +24,8 @@ const livros = [
     autor: "George Orwell",
     preco: "R$ 25,00",
     categoria: "Ficção Científica",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
   },
   {
     id: 3,
@@ -31,6 +35,8 @@ const livros = [
     autor: "Chris Tunwell, Fernando Acuña",
     preco: "R$ 19,99",
     categoria: "Didático",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
   },
   {
     id: 4,
@@ -40,6 +46,8 @@ const livros = [
     autor: "Robert Greene",
     preco: "R$ 23,99",
     categoria: "Autoajuda",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
   },
   {
     id: 5,
@@ -49,6 +57,52 @@ const livros = [
     autor: "David Flanagan",
     preco: "R$ 9,99",
     categoria: "Didático",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
+  },
+  {
+    id: 6,
+    banner: quarentaEoitoLeisDoPoder.src,
+    titulo: "As 48 leis do poder",
+    estado: "Moderado",
+    autor: "Robert Greene",
+    preco: "R$ 23,99",
+    categoria: "Autoajuda",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
+  },
+  {
+    id: 7,
+    banner: javascript.src,
+    titulo: "JavaScript: O Guia Definitivo",
+    estado: "Bom",
+    autor: "David Flanagan",
+    preco: "R$ 9,99",
+    categoria: "Didático",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
+  },
+  {
+    id: 8,
+    banner: quarentaEoitoLeisDoPoder.src,
+    titulo: "As 48 leis do poder",
+    estado: "Moderado",
+    autor: "Robert Greene",
+    preco: "R$ 23,99",
+    categoria: "Autoajuda",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
+  },
+  {
+    id: 9,
+    banner: javascript.src,
+    titulo: "JavaScript: O Guia Definitivo",
+    estado: "Bom",
+    autor: "David Flanagan",
+    preco: "R$ 9,99",
+    categoria: "Didático",
+    descricao: `Bentinho e Capitu cresceram juntos e se apaixonaram, mas a dúvida e o ciúme transformam a vida do narrador. 
+Considerado um dos maiores romances da literatura brasileira, Dom Casmurro explora a ambiguidade dos sentimentos humanos e a incerteza da verdade.`,
   },
 ];
 
