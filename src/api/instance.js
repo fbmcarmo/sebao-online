3e465a14af97661ef6d669938aff0aa4bd7c8502
fbmcarmo <fbrunomcarmo@gmail.com>
2025-06-16 08:49:39 -0300

    Deploy da API no render

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4002"
+    baseURL: "https://api-sebao-online.onrender.com"
 })
 
 instance.interceptors.request.use((config) => {
